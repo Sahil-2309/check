137c632e1a6657e56234c5a5b6c6d6468d9706f0
Sahil Arora <sahil.2723.a@gmail.com>
2025-07-13 14:37:07 +0530

    id added

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,9 @@ function App() {
         MUI TS App
       </Typography>
       <IndexStack>
-        <IndexLink href='#home'>Home</IndexLink>
+        <IndexLink href='#home' underline='none'>
+          Home
+        </IndexLink>
         <IndexLink href='#about'>About</IndexLink>
         <IndexLink href='#contact'>Contact</IndexLink>
       </IndexStack>
